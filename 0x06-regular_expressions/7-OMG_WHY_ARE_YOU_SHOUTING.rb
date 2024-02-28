2d0aec10434cf8db4df3c7b4de852a3a9ac2e173
TrentonFunt <sphinixphase17@gmail.com>
2024-02-28 01:01:26 +0100

    Finish Making regular Expressions-1

--- 0x06-regular_expressions/7-OMG_WHY_ARE_YOU_SHOUTING.rb
+++ 0x06-regular_expressions/7-OMG_WHY_ARE_YOU_SHOUTING.rb
@@ -4,6 +4,4 @@
 # This script should output: [SENDER],[RECEIVER],[FLAGS]
 
 regex = /(?<=from:|to:|flags:).+?(?=\])/
-input = ARGV[0]
-matches = input.scan(regex).join(',')
-puts matches
+puts ARGV[0].scan(regex).join(',')
